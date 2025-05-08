53b3f43ba48bec7a09bef13f5c9da78999f82644
neiranie <nurkhairanianneira@gmail.com>
2025-05-08 10:59:42 +0700

    commit 10

--- SinglyLinkedList.cpp
+++ SinglyLinkedList.cpp
@@ -9,12 +9,12 @@ class Node
     Node *next;
 };
 
-class SinglyLinkedList 
+class SingleLinkedList 
 {
     Node *START;
 
 public:
-    SinglyLinkedList()
+    SingleLinkedList()
     {
         START = NULL;
     }
@@ -110,3 +110,25 @@ public:
         }
     }
 };
+
+int main()
+{
+    SingleLinkedList mhs;
+    int nim;
+    char ch;
+    while(1)
+    {
+        cout << endl
+             << "Menu";
+        cout << endl
+             << "1. Menambah data kedalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua data didalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar" << endl;
+        cout << endl
+            << "Masukkan pilihan (1-5): ";
+        cin >> ch;
+        switch (ch)
+    }
+}
