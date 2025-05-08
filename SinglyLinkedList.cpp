ef39ffcd77ec25b97c091d7bd02fca4e220c1bb8
neiranie <nurkhairanianneira@gmail.com>
2025-05-08 11:37:17 +0700

    code fix

--- SinglyLinkedList.cpp
+++ SinglyLinkedList.cpp
@@ -170,19 +170,29 @@ int main()
             }
             Node *previous, *current;
             cout << endl
-                 << "Masukkan no mahasiswa yang dicari : ";
+                 << "Masukkan no mahasiswa yang dicari: ";
             cin >> nim;
             if (mhs.Search(nim, &previous, &current) == false)
                 cout << endl
                      << "Data tidak ditemukan" << endl;
             else
             {
-
-                
+                cout << endl
+                     << "Data ditemukan" << endl;
+                cout << "\nNo Mahasiswa: " << current->noMhs << endl;
+                cout << "\n";
             }
-                
-
-
         }
+        break;
+        case '5':
+        {
+            exit(0);
+        }
+        break;
+        default:
+        {
+            cout << "Pilihan salah !" << endl;
+        }
+    }
     }
 };
