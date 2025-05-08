f7d51b71d50b272fcfe636a400c9d0f51a1ebfa8
neiranie <nurkhairanianneira@gmail.com>
2025-05-08 11:19:56 +0700

    commit 14

--- SinglyLinkedList.cpp
+++ SinglyLinkedList.cpp
@@ -160,5 +160,21 @@ int main()
         {
             mhs.traverse();
         }
+        break;
+        case '4':
+        {
+            if (mhs.listEmpty() == true)
+            {
+                cout << "\nList Kosong\n";
+                break;
+            }
+            Node *previous, *current;
+            cout << endl
+                 << "Masukkan no mahasiswa yang dicari : ";
+            cin >> nim;
+                
+
+
+        }
     }
 };
