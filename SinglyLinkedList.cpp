056e529739e72305788641abf5ad3be7d990e1a6
neiranie <nurkhairanianneira@gmail.com>
2025-05-08 10:00:12 +0700

    commit 3

--- SinglyLinkedList.cpp
+++ SinglyLinkedList.cpp
@@ -2,8 +2,28 @@
 #include <string.h>
 using namespace std;
 
-class Node{
+class Node
+{
     public:
     int noMhs;
     Node *next;
 };
+
+class list 
+{
+    Node *START;
+
+public:
+    List()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << \"Masukkan Nomor Mahasiswa: ";
+        cin >> nim;
+
+    }
+}
