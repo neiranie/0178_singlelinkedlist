34fee153ee503b795f1fd2e2d7cd244f0b234781
neiranie <nurkhairanianneira@gmail.com>
2025-05-08 10:36:51 +0700

    commit 7

--- SinglyLinkedList.cpp
+++ SinglyLinkedList.cpp
@@ -9,12 +9,12 @@ class Node
     Node *next;
 };
 
-class List 
+class SinglyLinkedList 
 {
     Node *START;
 
 public:
-    List()
+    SinglyLinkedList()
     {
         START = NULL;
     }
@@ -60,5 +60,19 @@ public:
     bool listEmpty()
     {
         return START == NULL;
+    }
+
+    bool Search(int nim, Node **previous, Node **current)
+    {
+        *previous = START;
+        *current = START;
+
+        while ((current != NULL) && (nim != (*current)->noMhs))
+        {
+            *previous = *current;
+            *current = (*current)->next;
+        }
+
+        return (*current != NULL);
     }
 };
