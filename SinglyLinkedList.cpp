abd01e97d262da7c2d484609fe9d798a3323b1d1
neiranie <nurkhairanianneira@gmail.com>
2025-05-08 11:09:38 +0700

    commit 12

--- SinglyLinkedList.cpp
+++ SinglyLinkedList.cpp
@@ -146,6 +146,14 @@ int main()
                 break;
             }
             cout << endl
+                 << "\nMasukkan no mahasiswa yang akan dihapus : ";
+            cin >> nim;
+            if (mhs.delNode (nim) == false)
+                cout << endl
+                     << "Data tidak ditemukan" << endl;
+            else
+                cout << endl
+                     << "Data dengan nomor mahasiswa " << nim << " berhasil dihapus " << endl;
         }
     }
 };
