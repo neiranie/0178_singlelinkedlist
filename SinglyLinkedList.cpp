596ce5fe69bccc3f5a93b3c16b61ca10ceae8482
neiranie <nurkhairanianneira@gmail.com>
2025-05-08 10:06:46 +0700

    commit 4

--- SinglyLinkedList.cpp
+++ SinglyLinkedList.cpp
@@ -25,5 +25,16 @@ public:
         cout << \"Masukkan Nomor Mahasiswa: ";
         cin >> nim;
 
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if ((START != NULL ) && (nim == START->noMhs))
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+        }
     }
 }
